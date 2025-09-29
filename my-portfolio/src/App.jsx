ba3b6528b66264bc19e850f49635a8dcaa1cd2ed
lulumalta <luisadocarmo0305@gmail.com>
2025-09-29 17:05:04 -0300

    modificated changes

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,7 +1,7 @@
 import "./styles/global.scss"; // depois vamos criar esse arquivo
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import About from "./components/About";
+import About from "./components/about";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
